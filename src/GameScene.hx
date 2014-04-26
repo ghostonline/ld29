f4d080d207f81b0800f9fafb360adaa2c9e49310
Bart Veldstra <bart.veldstra@gmail.com>
2014-04-26 23:44:00 +0200

    Allow boats to be loaded from a level string

--- src/GameScene.hx
+++ src/GameScene.hx
@@ -7,7 +7,7 @@ class GameScene extends Scene
 	static inline var initialLife = 5;
 
 	var monster:Monster;
-	var ship:Ship;
+	var hangar:Hangar;
 	var destroyed:Array<Ship>;
 	var hud:Hud;
 	var life:Int;
@@ -29,15 +29,51 @@ class GameScene extends Scene
 		monster = new Monster(this);
 		add(monster);
 
-		ship = new Submarine(this);
-		add(ship);
+		hangar = new Hangar(this);
+	}
+
+	function loadLevel(level:String)
+	{
+		for (idx in 0...level.length)
+		{
+			var offscreenPadding = 25;
+			var offscreenX = HXP.random * (offscreenPadding * 2) - offscreenPadding;
+			var offscreenY = HXP.random * (offscreenPadding * 2) - offscreenPadding;
+			if (offscreenX > 0)
+			{
+				offscreenX += HXP.screen.width;
+			}
+			if (offscreenY > 0)
+			{
+				offscreenY += HXP.screen.height;
+			}
+
+			var shipWanderArea = new Rectangle(0, 0, HXP.screen.width, HXP.screen.height);
+			shipWanderArea.inflate(-50, -50);
+
+			var char = level.charAt(idx);
+			var instance:Ship = null;
+			switch(char)
+			{
+			case Level.bomber:
+				instance = hangar.bomber(offscreenX, offscreenY, shipWanderArea);
+			case Level.scanner:
+				instance = hangar.scanner(offscreenX, offscreenY, shipWanderArea);
+			case Level.miner:
+				instance = hangar.miner(offscreenX, offscreenY, shipWanderArea);
+			case Level.submarine:
+				instance = hangar.submarine(offscreenX, offscreenY, shipWanderArea);
+			}
+
+			if (instance != null) { add(instance); }
+		}
 	}
 
 	public function onShipDestroy(ship:Ship)
 	{
 		score += ship.score;
 		hud.setScore(score);
-		destroyed.push(ship);
+		remove(ship);
 	}
 
 	public function onMonsterHit(monster:Monster)
@@ -57,35 +93,7 @@ class GameScene extends Scene
 
 		HXP.screen.color = Palette.lightBlue;
 		monster.init(100, 100);
-		initShip(ship);
-	}
 
-	function initShip(ship:Ship)
-	{
-		var offscreenPadding = 25;
-		var offscreenX = HXP.random * (offscreenPadding * 2) - offscreenPadding;
-		var offscreenY = HXP.random * (offscreenPadding * 2) - offscreenPadding;
-		if (offscreenX > 0)
-		{
-			offscreenX += HXP.screen.width;
-		}
-		if (offscreenY > 0)
-		{
-			offscreenY += HXP.screen.height;
-		}
-
-		var shipWanderArea = new Rectangle(0, 0, HXP.screen.width, HXP.screen.height);
-		shipWanderArea.inflate(-50, -50);
-		ship.init(offscreenX, offscreenY, shipWanderArea, Ship.defaultHealth);
-	}
-
-	public override function update()
-	{
-		super.update();
-		while (destroyed.length > 0)
-		{
-			var ship = destroyed.pop();
-			initShip(ship);
-		}
+		loadLevel(Level.level1);
 	}
 }
